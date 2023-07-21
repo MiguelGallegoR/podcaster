fe82c5f2c53df00ea590a38c34801147c3917bc9
Miguel Gallego <miguel@Mac-mini-de-Miguel.local>
2023-07-21 14:45:54 +0200

    added comment

--- prueba-podcasts/src/components/SideBar.jsx
+++ prueba-podcasts/src/components/SideBar.jsx
@@ -1,14 +1,16 @@
 import {Divider, Card} from "antd"
 
 export const SideBar = ({singlePodcastInfo}) =>{
+    //CUANDO EL CARD ESTA VACÍO HACE BIEN EL FETCH, SI LE INCLUYES ESO FALLA
+    // <img src={singlePodcastInfo.artworkUrl600} alt="" />
+    //         <Divider />
+    //         <h4>{singlePodcastInfo.collectionName}</h4>
+    //         <p>by {singlePodcastInfo.artistName}</p>
+    //         <Divider />
+    //         <h4>Description:</h4>
+            
     return (
         <Card>
-            <img src={singlePodcastInfo.artworkUrl600} alt="" />
-            <Divider />
-            <h4>{singlePodcastInfo.collectionName}</h4>
-            <p>by {singlePodcastInfo.artistName}</p>
-            <Divider />
-            <h4>Description:</h4>
             
         </Card>
     )
