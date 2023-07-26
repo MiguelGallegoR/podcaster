791194db739fb432c55732b78ba91f9b9f619c2d
Miguel Gallego <miguel@Mac-mini-de-Miguel.local>
2023-07-26 14:50:11 +0200

    fixed sideBar component

--- prueba-podcasts/src/components/SideBar.jsx
+++ prueba-podcasts/src/components/SideBar.jsx
@@ -1,17 +1,15 @@
 import {Divider, Card} from "antd"
 
-export const SideBar = ({singlePodcastInfo}) =>{
-    //CUANDO EL CARD ESTA VACÍO HACE BIEN EL FETCH, SI LE INCLUYES ESO FALLA
-    // <img src={singlePodcastInfo.artworkUrl600} alt="" />
-    //         <Divider />
-    //         <h4>{singlePodcastInfo.collectionName}</h4>
-    //         <p>by {singlePodcastInfo.artistName}</p>
-    //         <Divider />
-    //         <h4>Description:</h4>
-            
+export const SideBar = ({singlePodcastInfo, singlePodcastInfoStorage }) =>{
     return (
-        <Card>
-            
+        <Card size="small" style={{width: 300}}>
+            <img src={singlePodcastInfo?.artworkUrl600} alt="" style={{width: 200}}/>
+            <Divider />
+            <h4>{singlePodcastInfo?.collectionName}</h4>
+            <p>by {singlePodcastInfo?.artistName}</p>
+            <Divider />
+            <h4>Description:</h4>
+            <p>{singlePodcastInfoStorage[0]?.summary.label}</p>
         </Card>
     )
 }
