3aa7de4b92105887e4eeae5a2b72f86e029ff3f6
Miguel Gallego <miguel@Mac-mini-de-Miguel.local>
2023-07-26 14:51:25 +0200

    added logic for podcast detail

--- prueba-podcasts/src/hooks/usePodcast.js
+++ prueba-podcasts/src/hooks/usePodcast.js
@@ -1,10 +1,94 @@
 import { useEffect, useState } from "react"
-import { getPodcastDetail } from "../services/getPodcastDetail"
+import { getPodcastDetail, getEpisodeList } from "../services/getPodcastDetail"
+
+let parseString = require('xml2js').parseString;
 export const usePodcast = (id) => {
+    const [isLoading, setIsLoading] = useState(true);
     const [podcast, setPodcast] = useState()
+    const [episodeList, setEpisodeList] = useState([])
+    const podcastEpisodesTimeStampKey = `podcastEpisodesTimeStamp${id}`;
+    const podcastEpisodesKey = `podcastEpisodes${id}`;
+
+    const createEpisodeObject = (item, index) =>
+    {
+        return {
+            title: item.title[0],
+            date: getFormatDate(item.pubDate[0]),
+            duration: item['itunes:duration'],
+            id: index,
+            soundContent: item['enclosure'][0]['$'].url,
+            description: item['description']
+        }
+    }
+
+    const getFormatDate = (stringDate) => 
+    {
+        const date = new Date(stringDate);
+
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+
+        return `${day}/${month}/${year}`;
+    }
+
+    // const getFormatDurationString = (duration) =>{
+    //     let durationString = '';
+    //     let totalSecs = Number(duration);
+    //     let secs = totalSecs % 60;
+    //     let min = Math.floor(totalSecs / 60);
+
+    //     if (min > 60)
+    //     {
+    //         let hours = Math.floor(min / 60);
+    //         durationString = hours + ":" + min + ":" + secs;
+    //         return durationString
+    //     }
+
+    //     durationString =  `${min}:${secs}`;
+    //     return durationString
+    // }
+
     useEffect(()=>{
-        getPodcastDetail(id).then(newPodcast => setPodcast(newPodcast))
+        async function fetchData(){
+            try{
+                const newPodcast = await getPodcastDetail(id)
+                setPodcast(newPodcast)
+            
+                const response = await getEpisodeList(newPodcast.feedUrl)
+                setIsLoading(false)
+                parseString(response, function (err, result) {
+                    const episodeListR = result?.rss?.channel[0]?.item;
+
+                    if (episodeListR){
+                        let episodeObjectList = [];
+                        let episodeIndex = 0;
+
+                        episodeListR.forEach(i => {
+                            episodeObjectList.push(createEpisodeObject(i, episodeIndex++));
+                        });
+                        setEpisodeList(episodeObjectList)
+                        localStorage.setItem(podcastEpisodesKey, JSON.stringify(episodeObjectList));
+                        localStorage.setItem(podcastEpisodesTimeStampKey, JSON.stringify((new Date()).getTime()));
+
+                    }
+
+                })
+                
+            }catch(error) {
+                console.error(error);
+                throw error;
+            }
+           
+        }
+        fetchData()
     },[id])
-    return {podcast}
+
+    const podcastsStorage = sessionStorage.getItem('cachedData')
+    const parsedStorage = JSON.parse(podcastsStorage)
+    const filteredPodcastStorage = [...parsedStorage.feed.entry].filter(p => p.id.attributes["im:id"] == id )
+
+    return {podcast, filteredPodcastStorage, episodeList, isLoading}
 }
+
 
