a39c6667ba8ffd971cbe62c07f3ce15fdff59c60
Miguel Gallego <miguel@Mac-mini-de-Miguel.local>
2023-07-26 14:52:11 +0200

    added columns, skeleton and table

--- prueba-podcasts/src/pages/Detail.jsx
+++ prueba-podcasts/src/pages/Detail.jsx
@@ -1,17 +1,54 @@
 import Header from "../components/Header"
+import { Table, Skeleton } from "antd"
 import { SideBar } from "../components/SideBar"
 import { usePodcast } from "../hooks/usePodcast"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 export const Detail = () => {
     const { id } = useParams();
-    const {podcast} = usePodcast(id)
-    console.log(podcast)
+    const {podcast, filteredPodcastStorage, episodeList, isLoading} = usePodcast(id)
+    console.log(episodeList[0].id)
+    const columns = [
+        {
+            title: 'Title',
+            dataIndex: 'title',
+            key: 'title',
+          },
+          {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+          },
+          {
+            title: 'Duration',
+            key: 'duration',
+            dataIndex: 'duration',
+          }
+    ]
     return (
         <>
             <Header />
-            <SideBar singlePodcastInfo={podcast} />
-            
+            {isLoading ? (
+                <Skeleton active />
+            ) : (
+                <main>
+                    <div className="totalEpisodes-container"><h2>Episodes: {episodeList.length}</h2></div>
+                    <div className="detail-container">
+                        <SideBar singlePodcastInfo={podcast} singlePodcastInfoStorage={filteredPodcastStorage}/>
+                        <Table
+                            dataSource={episodeList}
+                            columns={columns}
+                            onRow={(record, rowIndex) => {
+                                return {
+                                  onClick: (event) => {<Link to={`/podcast/${id}/episode/${episodeList.id}`}></Link>}, 
+                                };
+                              }}
+                        />
+                    </div>
+                </main>
+                
+            )}
         </>
+       
        
     )
     
