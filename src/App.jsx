b3426091721f42127692c3da4b2c168a397f2b99
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-08-02 11:37:30 +0200

    added Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import pathRoutes from './routes'
 import React from 'react'
+import Header from './components/Header'
 function App() {
   
   return (
     <BrowserRouter>
+
+      <Header />
       <Routes>
         { pathRoutes.map(({ element, path }) => {
             const Component = React.createElement(element);
