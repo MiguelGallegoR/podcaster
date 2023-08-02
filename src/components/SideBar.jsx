5cf16f037be53e158859dc056fedbea147ba3109
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-08-02 11:36:13 +0200

    added Link to podcast detail

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,19 @@
 import {Divider, Card} from "antd"
+import { Link } from 'react-router-dom';
 
-export const SideBar = ({singlePodcastInfo, singlePodcastInfoStorage }) =>{
+export const SideBar = ({ singlePodcastInfoStorage }) =>{
+    
+    console.log(singlePodcastInfoStorage)
     return (
         <Card size="small" style={{width: 300}}>
-            <img src={singlePodcastInfo?.artworkUrl600} alt="" style={{width: 200}}/>
+            <Link to={`/podcast/${singlePodcastInfoStorage[0]?.id.attributes["im:id"]}`} >
+                <img src={singlePodcastInfoStorage[0]?.["im:image"][2].label} alt="" style={{width: 200}}/>
+            </Link>
             <Divider />
-            <h4>{singlePodcastInfo?.collectionName}</h4>
-            <p>by {singlePodcastInfo?.artistName}</p>
+            <Link to={`/podcast/${singlePodcastInfoStorage[0]?.id.attributes["im:id"]}`} >
+                <h4>{singlePodcastInfoStorage[0]?.["im:name"].label}</h4>
+            </Link>
+            <p>by {singlePodcastInfoStorage[0]?.["im:artist"].label}</p>
             <Divider />
             <h4>Description:</h4>
             <p>{singlePodcastInfoStorage[0]?.summary.label}</p>
