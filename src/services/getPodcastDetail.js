545b83da17444bcac53e3566b249fde75e5cf94e
Miguel Gallego <gallegorodriguezmiguel58@gmail.com>
2023-08-02 11:20:48 +0200

    added function getEpisodeList

--- src/services/getPodcastDetail.js
+++ src/services/getPodcastDetail.js
@@ -12,16 +12,3 @@ export const getPodcastDetail = async(id) => {
     }
     
 }
-export const getEpisodeList = async (url) => {
-    try{
-        const res = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`${url}`)}`)
-        const json = await res.json()
-        const data = await json.contents
-        return data
-    
-       
-    
-    }catch (error) {
-        console.error('Error al realizar la solicitud:', error);
-    }
-}
